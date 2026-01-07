d780aec6e1e4c1ecd565d4346356fb36116bebe0
YUVADHESH M <yuvadheshm@gmail.com>
2026-01-07 22:43:12 +0530

    Time: 20 ms (34.26%) | Memory: 93.6 MB (41.18%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+class Solution{
+    public boolean containsDuplicate(int[]nums){
+        HashSet<Integer>set=new HashSet<>();
+        for(int n:nums){
+            if(set.contains(n)){
+                return true;
+            }
+            set.add(n);
+        }
+        return false;
+    }
+}
