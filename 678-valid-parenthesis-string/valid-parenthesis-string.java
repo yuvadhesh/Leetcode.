9a0a58003c87c0deef68f711cd98a395c0d8e620
YUVADHESH M <yuvadheshm@gmail.com>
2026-02-10 14:48:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (33.47%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,27 +1,25 @@
-class Solution {
-    public boolean checkValidString(String s) {
-        int low = 0, high = 0;
-        for (int i=0;i<s.length();i++) {
-            char c=s.charAt(i);
-            if (c == '(') {
+class Solution{
+    public boolean checkValidString(String s){
+        int low=0,high=0;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('){
                 low++;
                 high++;
-            } 
-            else if (c == ')') {
+            }
+            else if(c==')'){
                 low--;
                 high--;
-            } 
-            else { 
-                low--;  
+            }
+             else{ 
+                low--;
                 high++;
             }
-            if (high < 0) 
-            return false;
-           if(low<0){
-            low=0;
-           }
-           
-        }
+            if(high<0) 
+                return false;
+            if(low<0)
+            low = 0;     
+               }
         return low == 0;
     }
 }
